bf13de39ccae2a5ee413844eb54f990ca31220f8
Viktor Bengtsson <vibe16@student.bth.se>
2021-07-17 13:52:24 +0200

    Initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        electric: '#db00ff',
+        ribbon: '#0047ff',
+        phoneGreen: '#34c234',
+      },
+    },
+  },
+  plugins: [],
+};
